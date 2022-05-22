644e3d6ca96ad0285ef338add1ffb7760625fb3b
Sabelnikov <Artem.Sabelnikov@magenta-technology.com>
2022-05-22 12:10:02 +0400

    app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import {AppComponent} from './app.component';
+import { FiltersComponent } from './filters/filters.component';
+import { NetpointsComponent } from './netpoints/netpoints.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    FiltersComponent,
+    NetpointsComponent,
+    OrdersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
